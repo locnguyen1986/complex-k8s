beb4ed05c35746092561d1775104371bb77875b7
Nguyen Ngo <locnguyen1986@gmail.com>
2020-10-19 16:06:26 +0700

    fix image names

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ docker push locnguyen1986/complex-k8s-worker:$SHA
 echo '-------------------------------------------------------'
 echo 'APPLY CONFIG IN K8S'
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=locnguyen1986/complex-k8s-server$SHA
-kubectl set image deployments/client-deployment client=locnguyen1986/complex-k8s-client$SHA
-kubectl set image deployments/worker-deployment worker=locnguyen1986/complex-k8s-worker$SHA
+kubectl set image deployments/server-deployment server=locnguyen1986/complex-k8s-server:$SHA
+kubectl set image deployments/client-deployment client=locnguyen1986/complex-k8s-client:$SHA
+kubectl set image deployments/worker-deployment worker=locnguyen1986/complex-k8s-worker:$SHA
